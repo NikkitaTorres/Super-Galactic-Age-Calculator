0cb27a03a962752ffff3bdd8adc70e79cd33691e
NikkitaTorres <nikkitatorres@yahoo.com>
2023-11-13 14:13:35 -0800

    rewrote first test to check all ages

--- __tests__/Super-Galactic-Age-Calculator.test.js
+++ __tests__/Super-Galactic-Age-Calculator.test.js
@@ -1,10 +1,18 @@
 import { Age } from './../src/Super-Galactic-Age-Calculator.js';
 
 describe('Age', () => {
-  test('should correctly determine earth age', () => {
+  test('should correctly determine all ages', () => {
     const ageInstance = new Age(10);
     const earthAge = ageInstance.earthAge;
+    const mercuryAge = ageInstance.mercuryAge;
+    const venusAge = ageInstance.venusAge;
+    const marsAge = ageInstance.marsAge;
+    const jupiterAge = ageInstance.jupiterAge;
 
     expect(earthAge).toEqual(10);
+    expect(mercuryAge).toEqual(2.4);
+    expect(venusAge).toEqual(6.2);
+    expect(marsAge).toBeCloseTo(18.8, 1);
+    expect(jupiterAge).toEqual(118.6);
   });
 });
--- src/Super-Galactic-Age-Calculator.js
+++ src/Super-Galactic-Age-Calculator.js
@@ -1,9 +1,25 @@
 export class Age{
-  constructor(earthAge, mercuryAge, venusAge, marsAge, jupiterAge) {
+  constructor(earthAge) {
     this.earthAge = earthAge;
-    this.mercuryAge = mercuryAge;
-    this.venusAge = venusAge;
-    this.marsAge = marsAge;
-    this.jupiterAge = jupiterAge;
-  };
+    this.mercuryAge = this.calculateMercuryAge();
+    this.venusAge = this.calculateVenusAge();
+    this.marsAge = this.calculateMarsAge();
+    this.jupiterAge = this.calculateJupiterAge();
+  }
+
+  calculateMercuryAge() {
+    return this.earthAge * 0.24;
+  }
+
+  calculateVenusAge() {
+    return this.earthAge * 0.62;
+  }
+
+  calculateMarsAge() {
+    return this.earthAge * 1.88;
+  }
+
+  calculateJupiterAge() {
+    return this.earthAge * 11.86;
+  }
 }
